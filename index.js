0b5a2bd52c8cf033dca12d45a2697b5939b991b8
Saif Rafat <saif.rafat@gmail.com>
2022-03-09 14:12:40 +0600

    rest api

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const dishesRouter = require('./routes/dishes')
+const promotionsRouter = require('./routes/promotions')
+const leadersRouter = require('./routes/leaders')
+
+app.get('/', (req, res) => {
+    res.send("<h1>We are at Shop Door.Please Enter...</h1>")
+})
+app.use('/dishes', dishesRouter)
+app.use('/promotions', promotionsRouter)
+app.use('/leaders', leadersRouter)
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000... ')
+})
